45816e64c964e847ddbfdfb39408e700ea47ad9f
FelipePaterni <felipe.chaves2@etec.sp.gov.br>
2024-08-07 11:05:02 -0300

    arquivos

---
+++ siteTCC/contato.php
@@ -0,0 +1,7 @@
+<!--contato.php-->
+<div class="text-center" style="background-color: #eef1c6;">
+    <img src="./src/img/painelImg.png" class="img-fluid" alt="Imagem responsiva">
+</div>
+<div class="jumbotron contato-img jumbotron-fluid text-center">
+    <p class="contato-texto">EMAIL: TEAMMUSHMELO@OUTLOOK.COM</p>
+</div>
---
+++ siteTCC/footer.php
@@ -0,0 +1,4 @@
+<!--footer.php-->
+<div class="rodape-img  text-center ">
+    <p class="contato-texto display-5">&copy; 2024 Cabeças de Jaca. Todos os direitos reservados.</p>
+</div>
