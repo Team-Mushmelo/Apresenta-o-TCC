405bf3133fa986a883712aa733df2fff65ed9d6c
FelipePaterni <118012333+FelipePaterni@users.noreply.github.com>
2024-09-25 08:12:23 -0300

    clean

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Apresenta-o-TCC
 
-[Slide](https://www.canva.com/design/DAGNH7Jy768/7ybOZl5ctr0jZwfGH38vTQ/edit)
+[Slide]()
 
-[Site TCC ](http://localhost/siteTCC/)(Ligar o Xampp)
--- siteTCC/footer.php
+++
@@ -1,4 +0,0 @@
-<!--footer.php-->
-<div class="rodape-img  text-center ">
-    <p class="contato-texto display-5">&copy; 2024 Cabeças de Jaca. Todos os direitos reservados.</p>
-</div>
